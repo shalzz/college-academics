afd195a067ab3bc8da459209221f74773d621c6e
Shaleen Jain <shaleen.jain95@gmail.com>
2016-02-21 06:58:13 +0530

    Admob: add test device

--- attendance/src/main/java/com/shalzz/attendance/fragment/AdFragment.java
+++ attendance/src/main/java/com/shalzz/attendance/fragment/AdFragment.java
@@ -43,7 +43,7 @@ public class AdFragment extends Fragment {
         super.onActivityCreated(bundle);
         AdView mAdView = (AdView) getView().findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder()
-//                .addTestDevice("F9687B4F94B9427327A76A24D717C6B5")
+                .addTestDevice("F9687B4F94B9427327A76A24D717C6B5")
                 .build();
         mAdView.loadAd(adRequest);
     }
