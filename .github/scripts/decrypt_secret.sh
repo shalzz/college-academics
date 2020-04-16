19340e6810366b77df4eb577600aef48bf937672
Shaleen Jain <shaleen@jain.sh>
2020-04-16 17:45:03 +0530

    fix derp

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ../../play-service-account-key.json.2 play-service-account-key.json.gpg
+--output play-service-account-key.json play-service-account-key.json.gpg
